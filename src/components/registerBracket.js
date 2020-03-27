5943ff8ad1a4f7c0b780d05fb78ee994e58f29fa
kaiocon <kaiocon@users.noreply.github.com>
2020-03-27 19:19:35 +0000

    UNI Final

--- src/components/registerBracket.js
+++ src/components/registerBracket.js
@@ -22,6 +22,8 @@ class registerBracket extends React.Component {
         return(
             <div className="container mob">
                 <h1 className='aboutTitle'>Bracket Registration:</h1>
+                <div className='card' style={{textAlign: 'center'}}>
+                    <p>To register for this bracket, please checkout via the Paypal link below.</p>
                 <PayPalButton amount="0.01" options={{clientId: "AW0D2SYVJn0J47tWSx6qokjyk8epLkcHKscPz3u6JVT52mTSPWsESOtjFPCvz3p5iUu3X55jLLc_MZKy"}}
                     onSuccess={(details, data) => {
                         const {bracketID, registrant} = this.state;
@@ -29,11 +31,19 @@ class registerBracket extends React.Component {
                             if(res.status === 200){
                                 alert('Registered to bracket!');
                                 this.props.history.goBack();
-                            }else{alert('User already registered!')}
+                            }else if(res.status= 200){alert('User already registered!')}
+                        }).catch(()=>{
+                            alert('User already registered!');
+                            this.props.history.goBack();
                         })
 
                     }}
+                              catchError={() =>{
+                                  alert('User already registered!');
+                                  this.props.history.goBack();
+                              }}
                 />
+            </div>
             </div>
         )
     }
